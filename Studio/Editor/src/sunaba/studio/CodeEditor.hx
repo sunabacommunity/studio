821dd729da2bb57d44b1a2a06e66af7fc35edb46
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-07 12:09:45 -0900

    Add CodeEditor class and corresponding UI layout in SUML

---
+++ Studio/Editor/src/sunaba/studio/CodeEditor.hx
@@ -0,0 +1,14 @@
+package sunaba.studio;
+
+import sunaba.ui.Widget;
+import sunaba.ui.CodeEdit;
+
+class CodeEditor extends EditorWidget {
+    public override function init() {
+        load("studio://CodeEditor.suml");
+    }
+
+    public override function editorInit() {
+
+    }
+}
