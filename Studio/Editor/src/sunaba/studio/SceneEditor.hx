fed2f0da23db69f27edfffda21cc3f9de71837de
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-14 12:41:41 -0900

    Add SceneEditor class and layout for scene editing functionality

---
+++ Studio/Editor/src/sunaba/studio/SceneEditor.hx
@@ -0,0 +1,10 @@
+package sunaba.studio;
+
+import sunaba.ui.SpinBox;
+import sunaba.SubViewport;
+
+class SceneEditor extends EditorWidget {
+    public override function init() {
+        load("studio://SceneEditor.suml");
+    }
+}
--- Studio/Editor/src/sunaba/studio/fileHandlers/VscnFileHandler.hx
+++ Studio/Editor/src/sunaba/studio/fileHandlers/VscnFileHandler.hx
@@ -1,10 +1,16 @@
 package sunaba.studio.fileHandlers;
 
 import sunaba.studio.explorer.FileHandler;
+import sunaba.studio.SceneEditor;
 
 class VscnFileHandler extends FileHandler {
     public override function init() {
         this.extension = "vscn";
         this.iconPath = "studio://icons/16/clapperboard.png";
+    }
+
+    public override function openFile(path: String) {
+        var sceneEditor = new SceneEditor(editor, EditorArea.workspace);
+        editor.setWorkspaceTabIcon(sceneEditor, explorer.loadIcon(iconPath));
     }
 }
