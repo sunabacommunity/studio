3dc1b7047c69d6c76cc1ae84cbf0adcafdf3799a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-02 10:24:46 -0900

    added toolchain directory function

--- Studio/StudioUtils.cs
+++ Studio/StudioUtils.cs
@@ -1,10 +1,55 @@
 using Godot;
 using System;
+using System.Runtime.InteropServices;
 
 public partial class StudioUtils : Node
 {
 	public string GetBaseDirectory()
 	{
 		return AppDomain.CurrentDomain.BaseDirectory;
+	}
+
+	public string GetToolchainDirectory()
+	{
+		var baseDir = GetBaseDirectory();
+		var toolchainDirectory = baseDir + "/toolchain/";
+		if (OS.GetName() == "Windows")
+		{
+			toolchainDirectory += "windows-x86_64/";
+		}
+		else if (OS.GetName() == "Linux")
+		{
+			toolchainDirectory += "linux";
+			if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+			{
+				toolchainDirectory += "-x86_64/";
+			}
+			else
+			{
+				return "";
+			}
+		}
+		else if (OS.GetName() == "macOS")
+		{
+			toolchainDirectory += "mac";
+			if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+			{
+				toolchainDirectory += "-x86_64/";
+			}
+			else if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+			{
+				toolchainDirectory += "-arm64";
+			}
+			else
+			{
+				return "";
+			}
+		}
+		else
+		{
+			return "";
+		}
+
+		return toolchainDirectory;
 	}
 }
