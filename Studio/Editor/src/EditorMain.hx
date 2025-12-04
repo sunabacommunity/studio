9cd746ea889798b8f0cd3769972fee1bd6cf7b71
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-04 09:25:45 -0900

    Add missing import for sys.FileSystem in EditorMain

--- Studio/Editor/src/EditorMain.hx
+++ Studio/Editor/src/EditorMain.hx
@@ -3,6 +3,7 @@ import sunaba.App;
 import sunaba.studio.Editor;
 import sunaba.studio.StudioUtils;
 import sunaba.OSService;
+import sys.FileSystem;
 
 class EditorMain extends App {
     public static function main() {
