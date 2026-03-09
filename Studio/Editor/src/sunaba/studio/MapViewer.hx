27d657c4df07f0b556453f343547ffee36e7f092
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-08 23:18:25 -0800

    Updated MapViewer to use TrenchBroom

--- Studio/Editor/src/sunaba/studio/MapViewer.hx
+++ Studio/Editor/src/sunaba/studio/MapViewer.hx
@@ -58,7 +58,7 @@ class MapViewer extends EditorWidget {
         });
         editButton = getNodeT(Button, "vbox/toolbar/hbox/edit");
         editButton.pressed.add(() -> {
-            getEditor().openNetRadiant(io.getFilePath(filePath));
+            getEditor().openTrenchbroom(io.getFilePath(filePath));
         });
 
         throbberParent = getNodeT(Control, "vbox/toolbar/hbox/throbber");
